33c9926631f634f8d2a0ef9d9b686372cb1dbc96
Aryan Raj <arajjnv@gmail.com>
2023-12-23 18:24:05 +0530

    mongodb connection

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// require('dotenv').config({path: './env'})
+
+import dotenv from "dotenv"
+
+// import mongoose from "mongoose"
+import { DB_NAME } from "./constants.js"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express"
+
+// (async () => {
+//     try{
+//         await mongoose.connect($`{process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.log("ERROR:", error);
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`APP is listening on port: ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.log("Error:", error)
+//     }throw error
+// })()
