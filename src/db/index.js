c608b54d5bb3a498badfec289ff977d34b550516
Aryan Raj <arajjnv@gmail.com>
2023-12-26 13:20:31 +0530

    commit

--- src/db/index.js
+++ src/db/index.js
@@ -1,13 +1,12 @@
-import mongoose from "mongoose"
-import { DB_NAME } from "../constants.js"
-const connectDB = async () => {
-    try {
+import mongoose from "mongoose"     
+import { DB_NAME } from "../constants.js"   
+const connectDB = async () => {     
+    try {     
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`MOngoDB connected!! HOST: ${connectionInstance.connection.host}`)
-    } catch (error) {
-        console.log(error)
-        process.exit(1)
-    }
+    } catch (error) {       
+        console.log(error)     
+      }    
 }
 
-export default connectDB
+export default connectDB       
--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ dotenv.config({
     path: './env'
 })
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is listening on port: ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log(error)
+})
 
 
 
